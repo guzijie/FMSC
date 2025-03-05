2456b3bd1e972f299e093fd4ed978b01f8565e76
guzijie <92509446+guzijie@users.noreply.github.com>
2025-03-05 19:22:26 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,7 +1,4 @@
-"""
-Authors: Wouter Van Gansbeke, Simon Vandenhende
-Licensed under the CC BY-NC 4.0 license (https://creativecommons.org/licenses/by-nc/4.0/)
-"""
+
 import argparse
 import torch
 import yaml
